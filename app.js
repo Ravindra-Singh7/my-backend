268773cebddb6471d00dab124b13b4e6507f32dc
ravi <rc0422754@gmail.com>
2026-06-20 08:24:18 +0530

    cors fix

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://my-backend:ravi123@cluster0.jplq5sh.mongodb.net/?appName=Cluster0")
+  .then(() => { console.log("Database connect ho gaya!"); })
+  .catch((error) => { console.log("Error:", error); });
 
 const authRoutes = require("./routes/auth");
 const subscriptionRoutes = require("./routes/subscription");
 
-const app = express();
+app.use("/", authRoutes);
+app.use("/", subscriptionRoutes);
 
-app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URL)
-.then(() => {
-  console.log("Database connect ho gaya!");
-})
-.catch((error) => {
-  console.log("Error:", error);
-});
-
-app.use(authRoutes);
-
-app.use(subscriptionRoutes);
-// Global Error Handler
 app.use((err, req, res, next) => {
   console.log("Error:", err.message);
   res.status(500).json({ message: "Kuch galat ho gaya!", error: err.message });
